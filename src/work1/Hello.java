79733eb1abbdd01bafdf2004ffb900c27a0e6b4b
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-06 18:56:20 -0500

    Created more commit

--- src/work1/Hello.java
+++ src/work1/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello friends");
         System.out.println("Created Git Repository");
+        System.out.println("Did more commit");
     }
 }
