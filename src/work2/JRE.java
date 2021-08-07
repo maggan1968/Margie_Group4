7757559050664cdaaa74dc38d3371149de23395f
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-07 14:06:26 -0500

    What stand JRE for ?

---
+++ src/work2/JRE.java
@@ -0,0 +1,14 @@
+package work2;
+
+public class JRE {
+
+}
+/*
+The Java Runtime Environment (JRE) is what you get when you download
+Java software. The JRE consists of the Java Virtual Machine (JVM),
+Java platform core classes, and supporting Java platform libraries.
+The JRE is the runtime portion of Java software, which is all you need
+to run it in your Web browser.
+
+
+ */
