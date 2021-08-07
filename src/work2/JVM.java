45bf75f65a24420dc4fcbe14edea19d99d5521e6
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-07 14:16:53 -0500

    What stand JVM for ?

---
+++ src/work2/JVM.java
@@ -0,0 +1,13 @@
+package work2;
+
+public class JVM {
+}
+
+/*
+A Java virtual machine (JVM) is a virtual machine that enables a computer to run
+Java programs as well as programs written in other languages that are also compiled to
+Java bytecode. The JVM is detailed by a specification that formally describes what is
+required in a JVM implementation.
+
+
+ */
