064f05fbfb010c6c254d2449417514c3f0c3e7bc
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-08-07 14:15:01 -0500

    What stand JDK for ?

---
+++ src/work2/JDK.java
@@ -0,0 +1,13 @@
+package work2;
+
+public class JDK {
+
+}
+/*
+The Java Development Kit (JDK) is a software development environment used for
+developing Java applications. It includes the Java Runtime Environment (JRE),
+an interpreter/loader (java), a compiler (javac), an archiver (jar),
+a documentation generator (javadoc) and other tools needed in Java development.
+
+
+ */
